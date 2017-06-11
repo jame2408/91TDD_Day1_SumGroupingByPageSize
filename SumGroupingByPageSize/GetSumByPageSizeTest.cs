b16e0a406d2c9aacd9872af0d3e5ac2da9957bb4
jame2408 <jame2408@gmail.com>
2017-06-11 17:19:45 +0800

    Add first unit test case 「Pagesize_is_3_and_Sum_Cost_Should_be_6_15_24_21」(red)

--- SumGroupingByPageSize/GetSumByPageSizeTest.cs
+++ SumGroupingByPageSize/GetSumByPageSizeTest.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SumGroupingByPageSize
@@ -7,8 +10,37 @@ namespace SumGroupingByPageSize
     public class GetSumByPageSizeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Pagesize_is_3_and_Sum_Cost_Should_be_6_15_24_21()
         {
+            //Arrange
+            var target = new GroupingByPageSize();
+
+            //Act
+            var actual = target.GetSum(pagesize: 3, groupName: nameof(Order.Cost)).ToList();
+
+            //Assert
+            var expected = new List<int>{ 6, 15, 24, 21};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    internal class Order
+    {
+        public int Id { get; set; }
+
+        public int Cost { get; set; }
+
+        public int Revenue { get; set; }
+
+        public int SellPrice { get; set; }
+
+    }
+
+    class GroupingByPageSize
+    {
+        public IEnumerable<int> GetSum(int pagesize, string groupName)
+        {
+            throw new NotImplementedException();
         }
     }
 }
