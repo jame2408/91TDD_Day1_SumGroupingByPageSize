a6e95b173b3ad8fbeb81da38569be3a86c0f5723
jame2408 <jame2408@gmail.com>
2017-06-11 18:45:57 +0800

    Pass test case 「Pagesize_is_3_and_Sum_Cost_Should_be_6_15_24_21」 and code refactoring.

--- SumGroupingByPageSize/GetSumByPageSizeTest.cs
+++ SumGroupingByPageSize/GetSumByPageSizeTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -13,13 +12,13 @@ namespace SumGroupingByPageSize
         public void Pagesize_is_3_and_Sum_Cost_Should_be_6_15_24_21()
         {
             //Arrange
-            var target = new GroupingByPageSize();
+            var target = new Order();
 
             //Act
-            var actual = target.GetSum(pagesize: 3, groupName: nameof(Order.Cost)).ToList();
+            var actual = target.GetOrders().GetSum(pagesize: 3, groupName: s => s.Cost).ToList();
 
             //Assert
-            var expected = new List<int>{ 6, 15, 24, 21};
+            var expected = new List<int> { 6, 15, 24, 21 };
             CollectionAssert.AreEqual(expected, actual);
         }
     }
@@ -34,13 +33,38 @@ namespace SumGroupingByPageSize
 
         public int SellPrice { get; set; }
 
+        internal IEnumerable<Order> GetOrders()
+        {
+            var orders = new List<Order>()
+            {
+                new Order() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21},
+                new Order() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22},
+                new Order() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23},
+                new Order() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24},
+                new Order() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25},
+                new Order() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26},
+                new Order() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27},
+                new Order() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28},
+                new Order() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29},
+                new Order() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30},
+                new Order() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31}
+            };
+            return orders;
+        }
     }
 
-    class GroupingByPageSize
+    static class GroupingByPageSize
     {
-        public IEnumerable<int> GetSum(int pagesize, string groupName)
+        public static IEnumerable<int> GetSum<T>(this IEnumerable<T> data, int pagesize, Func<T, int> groupName)
         {
-            throw new NotImplementedException();
+            var orders = data.ToList();
+
+            var index = 0;
+            while (index <= orders.Count)
+            {
+                yield return orders.Skip(index).Take(pagesize).Sum(groupName);
+                index += pagesize;
+            }
         }
     }
 }
